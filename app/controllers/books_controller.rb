ed82200d92717f36b5def9e1af2b2ff789f10a37
ko <kyokucho1989@gmail.com>
2023-09-21 06:01:06 +0900

    ページのソート順と表示数変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
 
   # GET /books or /books.json
   def index
-    @books = Book.all.page(params[:page]).order(id: :desc)
+    @books = Book.all.page(params[:page]).order(id: :asc)
   end
 
   # GET /books/1 or /books/1.json
