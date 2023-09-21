6519c5423979eb6c7d326a3c2dd86b436d558a21
ko <kyokucho1989@gmail.com>
2023-09-22 05:59:12 +0900

    Usermodelにカラム追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :address
+    devise_parameter_sanitizer.for(:sign_up) << :postal_code
+    devise_parameter_sanitizer.for(:sign_up) << :self_introduce
+  end
 end
