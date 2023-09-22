8347a39927dbd44d7bb137f4f7d4f38c4a18feb2
ko <kyokucho1989@gmail.com>
2023-09-23 07:49:17 +0900

    ログインログアウト後のルーティング変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:adress, :self_introduce, :postal_code])
   end
+
+  def after_sign_in_path_for(resource_or_scope)
+    books_path
+  end
+
+  def after_sign_out_path_for(resource)
+    books_path
+  end
 end
