74ef1ebfc13983241ed7ed7769daf31134c2cb87
ko <kyokucho1989@gmail.com>
2023-09-24 06:28:21 +0900

    ユーザー情報編集画面追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,14 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:adress, :self_introduce, :postal_code])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[address self_introduce postal_code])
   end
 
-  def after_sign_in_path_for(resource_or_scope)
+  def after_sign_in_path_for(_resource_or_scope)
     books_path
   end
 
-  def after_sign_out_path_for(resource)
+  def after_sign_out_path_for(_resource)
     books_path
   end
 end
