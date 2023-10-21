97bfa82bef597795cb7b15f160fa40243b281750
ko <kyokucho1989@gmail.com>
2023-10-22 06:21:01 +0900

    日報詳細の画面作成

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,24 @@
 # frozen_string_literal: true
 
 class ReportsController < ApplicationController
+  before_action :set_report, only: %i[show edit update destroy]
+
   def index
     @reports = Report.includes(:user).order(:id).page(params[:page])
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update; end
+
+  def destroy; end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_report
+    @report = Report.find(params[:id])
   end
 end
