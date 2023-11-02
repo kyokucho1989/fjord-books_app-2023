c637b4df78d91d29d3f2d99e5d3674e90a05595c
ko <kyokucho1989@gmail.com>
2023-11-03 05:08:35 +0900

    コメント削除機能追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,6 +21,32 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    if params[:report_id].present?
+      @report = Report.find(params[:report_id])
+      redirect_path = report_url(@report)
+      redirect_controller = 'reports'
+    else
+      @book = Book.find(params[:book_id])
+      redirect_path = book_url(@book)
+      redirect_controller = 'books'
+    end
+
+    @comment = Comment.find(params[:id])
+
+    if @comment.user_id != current_user.id
+      redirect_to controller: redirect_controller, action: 'show'
+      return
+    end
+
+    @comment.destroy
+
+    respond_to do |format|
+      format.html { redirect_to redirect_path, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human) }
+      format.json { head :no_content }
+    end
+  end
+
   private
 
   def comment_params
