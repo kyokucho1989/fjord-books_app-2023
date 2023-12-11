54314cf42d61067b1beb444c11df527c4e9931c4
ko <kyokucho1989@gmail.com>
2023-12-12 05:31:50 +0900

    fixture修正/reportモデルのテスト追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,28 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'editable' do
+    user1 = users(:alice)
+    user2 = users(:bob)
+    user1.reports.create!(title: 'title', content: 'hello')
+    assert user1.reports.first.editable?(user1)
+    assert_not user1.reports.first.editable?(user2)
+  end
+
+  test 'create_on' do
+    user1 = users(:alice)
+    user1.reports.create!(title: 'title', content: 'hello')
+    report1 = user1.reports.first
+    assert_equal(report1.created_on, report1.created_at.to_date)
+  end
+
+  test 'save_mention' do
+    user1 = users(:alice)
+    user2 = users(:bob)
+    report1 = user1.reports.create!(title: '言及', content: 'hello')
+    content = "http://localhost:3000/reports/#{report1.id}"
+    report2 = user2.reports.create!(title: '言及', content:)
+    assert_includes(report2.mentioning_reports, report1)
+    assert_not_includes(report1.mentioning_reports, report2)
+  end
 end
