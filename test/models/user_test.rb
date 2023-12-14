c9a108c9897bdb054cdfcb7b4ccac6cdf583f59d
ko <kyokucho1989@gmail.com>
2023-12-14 21:04:28 +0900

    日報作成と修正と削除のテスト追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,7 +6,7 @@ class UserTest < ActiveSupport::TestCase
   test 'name_or_email_exists' do
     user1 = users(:alice)
     user2 = users(:carol)
-    assert_equal(user1.name_or_email,user1.email)
-    assert_equal(user2.name_or_email,user2.name)
+    assert_equal(user1.name_or_email, user1.email)
+    assert_equal(user2.name_or_email, user2.name)
   end
 end
--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -4,44 +4,37 @@ require 'application_system_test_case'
 
 class ReportsTest < ApplicationSystemTestCase
   setup do
+    @user = users(:alice)
     @report = reports(:one)
+    visit new_user_session_url
+    fill_in 'user_email', with: @user.email
+    fill_in 'user_password', with: 'password'
+    click_on 'ログイン'
+    assert_text 'ログインしました'
   end
 
-  test 'visiting the index' do
+  test 'should create Report' do
     visit reports_url
-    assert_selector 'h1', text: 'Reports'
-  end
-
-  test 'should create report' do
-    visit reports_url
-    click_on 'New report'
-
-    fill_in 'Content', with: @report.content
-    fill_in 'Title', with: @report.title
-    fill_in 'User', with: @report.user_id
-    click_on 'Create Report'
-
-    assert_text 'Report was successfully created'
-    click_on 'Back'
+    click_on '日報'
+    click_on '日報の新規作成'
+    fill_in 'report[title]', with: @report.title
+    fill_in 'report[content]', with: @report.content
+    click_on '登録する'
+    assert_text '日報が作成されました'
   end
 
   test 'should update Report' do
     visit report_url(@report)
-    click_on 'Edit this report', match: :first
-
-    fill_in 'Content', with: @report.content
-    fill_in 'Title', with: @report.title
-    fill_in 'User', with: @report.user_id
-    click_on 'Update Report'
-
-    assert_text 'Report was successfully updated'
-    click_on 'Back'
+    click_on 'この日報を編集'
+    fill_in 'report[title]', with: '内容'
+    fill_in 'report[content]', with: '修正した。'
+    click_on '更新する'
+    assert_text '日報が更新されました'
   end
 
   test 'should destroy Report' do
     visit report_url(@report)
-    click_on 'Destroy this report', match: :first
-
-    assert_text 'Report was successfully destroyed'
+    click_on 'この日報を削除', match: :first
+    assert_text '日報が削除されました'
   end
 end
